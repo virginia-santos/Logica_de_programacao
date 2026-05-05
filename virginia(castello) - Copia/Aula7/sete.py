df139d1cabeeed563cb6a100750b255f0066d41f
virginia santos <vivisarahdessantos@gmail.com>
2026-05-05 11:27:05 -0300

    Aula 05.05.26

---
+++ virginia(castello) - Copia/Aula7/sete.py
@@ -0,0 +1,51 @@
+# projeto cancela automática 
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+print("Bem vindo ao ShoppingCenter")
+print("Qual opcão você deseja?")
+print("1 -Ticket n/ 2- Tag n/ 3- Interfone")
+metodo_entrada = input("Ticket / Tag / Interfone")
+
+if metodo_entrada == "Ticket":
+    print("Bem vindo ao ShoppingCenter")
+    hora_entrada = float(input("Digite o horário de entrada"))
+    valor_estacionamento = float(input("Digite o valor a ser cobrado"))
+    hora_saida = float(input("Digite o horário de chegada"))
+    tempo_permanencia = hora_saida - hora_entrada
+
+elif metodo_entrada == "Tag":
+    print("Bem vindo ao ShoppingCenter")
+    print("Sua permanência será cobrada na sua fatura")
+
+elif metodo_entrada == "Interfone":
+    print("Bem vindo ao ShoppingCenter")
+    print("Liberando acesso pelo Interfone")
+    print("Sua saída deverá ser feita também pelo Interfone")
+
+else:
+    print("Obrigado pela visita")
